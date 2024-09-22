d42903460ba125dcbeb7344c4ed847abe6e5867f
mr-green1337 <islam.hachimi2003@gmail.com>
2024-09-22 02:34:16 +0200

    Basic data_collection service

---
+++ data_collection/src/app.py
@@ -0,0 +1,16 @@
+import os
+from loguru import logger
+from quixstreams import Application
+
+def get_app() -> Application:
+
+    logger.info("Creating application...")
+    app = Application(
+        broker_address=os.environ["KAFKA_BROKER_ADDRESS"],
+        consumer_group="json__trade_to_ohlc_consumer_group",
+        auto_offset_reset="earliest",
+        consumer_extra_config={"allow.auto.create.topics": "true"},
+        producer_extra_config={"allow.auto.create.topics": "true"},
+    )
+
+    return app
--- data_collection/src/main.py
+++ data_collection/src/main.py
@@ -1,2 +1,50 @@
+from datetime import timedelta
+import os
+from loguru import logger
+
+from src.app import get_app
+
+TIME_WINDOW_SIZE = 30
+
+def reduce_data(data, new_data):
+    """
+    Function to reduce data in a window
+    """
+    return data + new_data
+
+def init_data():
+    """
+    Function to initialize data
+    """
+    return 0
+
 def run():
-    pass
+    """
+    Retrive Data from an API and send it to a Kafka Topic
+    """
+
+    app = get_app()
+
+    logger.info("Setting input topic for data_collection service")
+    input_topic = app.topic(os.environ["KAFKA_INPUT_TOPIC"])
+
+    logger.info("Setting output topic for data_collection service")
+    output_topic = app.topic(os.environ["KAFKA_OUTPUT_TOPIC"])
+
+    # Creating a streaming dataframe receiver
+    sdf = app.dataframe(input_topic)
+
+    sdf.tumbling_window(timedelta(seconds=TIME_WINDOW_SIZE)) \
+    .reduce(reduce_data, init_data) \
+    .final()
+
+    # Play some magic on data before sending to output topic
+    #...
+
+    sdf = sdf.to_topic(output_topic)
+
+    # Start the application
+    app.run(sdf)
+
+if __name__ == "__main__":
+    run()
